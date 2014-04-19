179e997226e13cf2eeb6b40a132240f17cbbc8ff
John Haberstroh <jhaberstroh@berkeley.edu>
2014-04-18 18:14:44 -0700

    Merged f3Plotter into f2_1TrackModes, updated 2d-histogram feature, updated interface to use argparse for runtime args instead of configparser, updated TrackModes to be able to function for situations where the full timeseries cannot be loaded into memory

--- scripts/pyPCA/f2_1TrackModes.py
+++ scripts/pyPCA/f2_1TrackModes.py
@@ -11,6 +11,32 @@ from f1SidechainCorr import h5tstag, h5crtag
 import ConfigParser
 import argparse
 
+# -----------------------------OLD CODE :(---------------------------------------
+
+def dEModesSeparate(dEmodes_i_tn, dEtot_t_i, n_modes, i = 0):
+	assert(type(dEmodes_i_tn) == np.ndarray)
+	assert(n_modes <= dEmodes_i_tn.shape[2])
+	Ntimes = dEmodes_i_tn.shape[1]
+
+	timeseries_sum = np.zeros(Ntimes)
+
+	plots = []
+	legend = []
+	for mode_counter in xrange(n_modes):
+		mode = -mode_counter - 1
+		timeseries_mode = dEmodes_i_tn[i,:,mode]
+
+		p1, = plt.plot(timeseries_mode)
+		plots.append(p1)
+		legend.append("Mode "+str(mode_counter+1))
+
+
+	plt.legend(plots,legend)
+	plt.savefig("plot_timesep_m"+str(n_modes)+"s"+str(i+1)+".png")
+	plt.savefig("plot_timesep_m"+str(n_modes)+"s"+str(i+1)+".pdf")
+	#plt.show()
+
+
 
 
 def ModeTracker(E_t_ij, modes_inj):
@@ -100,78 +126,102 @@ def ComputeModes(corr, cutoffFactor = 1E-4):
 	return vi, wi, impact_i
 
 
-def PlotLogSpectrum(vi, impact_i, floor = 1E-15, plottype = None, fnamebase="plot"):
+# -----------------------------NEW CODE!---------------------------------------
+
+def PlotLogSpectrum(site, vi, impact_i, floor = 1E-15, plottype = None, fnamebase="plot"):
     assert(plottype != None)
-    Ni = len(vi)
-    for i in xrange(Ni):
-        v = np.array([abs((x * imp)) + floor for x, imp in zip(vi[i], impact_i[i])])
-        v = v[::-1]
-        v = v[0:-3]
-        #floor /= v[0]
-        #v /= v[0]
-        v = np.sqrt(v)
-        vlog = np.log10(v)
-        plt.plot(vlog, 'ro')
-        #plt.plot((math.log(floor,10),) * len(vi[i]))
+    v = np.array([abs((x * imp)) + floor for x, imp in zip(vi[i], impact_i[i])])
+    v = v[::-1]
+    v = v[0:-3]
+    #floor /= v[0]
+    #v /= v[0]
+    v = np.sqrt(v)
+    vlog = np.log10(v)
+    plt.plot(vlog, 'ro')
+    #plt.plot((math.log(floor,10),) * len(vi[i]))
 
-        if plottype == 'pdf':
-            plt.savefig("{}_spect{}.pdf".format(fnamebase,i+1))
-            plt.clf()
-        if plottype == 'png':
-            plt.savefig("{}_spect{}.png".format(fnamebase,i+1))
-            plt.clf()
-        if plottype == 'display':
-            plt.show()
+    if plottype == 'pdf':
+        plt.savefig("{}_spect{}.pdf".format(fnamebase,site))
+        plt.clf()
+    if plottype == 'png':
+        plt.savefig("{}_spect{}.png".format(fnamebase,site))
+        plt.clf()
+    if plottype == 'display':
+        plt.show()
 
+def Plot2DHist(x, y, site=None, mode1=None, mode2=None, plottype = None, fnamebase = "plot"):
+    if fnamebase == "":
+        fnamebase = "plot"
+    xwidth = (max(x) - min(x)) / 2. * 1.10
+    xmid   = (max(x) + min(x)) / 2.
+    ywidth = (max(y) - min(y)) / 2. * 1.10
+    ymid   = (max(y) + min(y)) / 2.
+    xedges = np.linspace(xmid - xwidth, xmid + xwidth, 50)
+    yedges = np.linspace(ymid - ywidth, ymid + ywidth, 50)
+    
+    print "Saving histogram figure..."
+    H, xedges, yedges = np.histogram2d(x, y, bins=(xedges,yedges))
+    im = plt.imshow(H, interpolation='nearest', origin='low', extent=[xedges[0],xedges[-1],yedges[0],yedges[-1]])
+    if plottype=='png':
+        plt.savefig("{}_2d_s{}_{}v{}.png".format(fnamebase,site,mode1,mode2))
+        plt.clf()
+    if plottype=='pdf':
+        plt.savefig("{}_2d_s{}_{}v{}.pdf".format(fnamebase,site,mode1,mode2))
+        plt.clf()
+    if plottype=='display':
+        plt.show()
 
+def PlotDeltaTimeseries(dEmodes_i_tn, dEtot_t_i, n_modes, i = 0):
+	assert(type(dEmodes_i_tn) == np.ndarray)
+	assert(n_modes <= dEmodes_i_tn.shape[2])
+	Ntimes = dEmodes_i_tn.shape[1]
 
-def DeltaModeTracker(E_t_ij, modes_inj):
+	timeseries_sum = np.zeros(Ntimes)
+	for mode_counter in xrange(n_modes):
+		mode = -mode_counter - 1
+		timeseries_mode = dEmodes_i_tn[i,:,mode]
+
+		timeseries_sum += timeseries_mode
+
+	timeseries_sum -= dEtot_t_i[:,i]
+
+	p1, = plt.plot(timeseries_sum)
+	p2, = plt.plot(dEtot_t_i[:,i])
+	plt.legend([p1,p2],["Difference from dEtot", "dEtot"])
+	#plt.plot(dEtot_t_i[:,i])
+	plt.savefig("plot_timetot_m"+str(n_modes)+"s"+str(i+1)+".png")
+	plt.savefig("plot_timetot_m"+str(n_modes)+"s"+str(i+1)+".pdf")
+	#plt.show()
+
+def DeltaModeTracker(E_t_ij, modes_inj, site, modes_requested=[]):
 	"""
 	DeltaModeTracker: Takes E(t) np.ndarray and an iterable of modes, and plots the deviation of dE(t) [after taking the average for each site]
 	                  as contributed from the modes, as compared to the actual dE(t)
 
 	\input 	E_t_ij 		- a collection of Eij as a function of time
-		modes_inj 	- a collection of n normalized modes (in j coordinates) for each site i
+		modes_inj 	- a collection of n normalized modes (in j coordinates) for each site i -- rotation matrix between bases
 
-	\output DeltaEm_t_in	- a collection of the variations in energetics for the n modes for site i as a function of time
+	\output dDE_t	        - a list, for (site,mode) pairs (i,n) requested, of 1d arrays tracking those modes in time
 	"""
 
 	print "Running DeltaModeTracker..."
-	E_t_ij = np.array(E_t_ij)
+	Ntimes  = E_t_ij.shape[0]
+	Nsites  = E_t_ij.shape[1]
+	Ncoarse = E_t_ij.shape[2]
 
-	DeltaEmodes_t_in = []
-	DeltaEtot_t_i = []
-
-	Ntimes = len(E_t_ij)
-	Nsites = len(E_t_ij[0])
-	Ncoarse = len(E_t_ij[0][0])
+        # Flip the modes so that they are in order from largest to smallest and zero-based 
 
 	E_avg_ij = np.zeros( (Nsites, Ncoarse) )
 	print "Ntimes: ", Ntimes
 	print "Nsites: ", Nsites
 	print "Ncoarse: ", Ncoarse
-	print "Averages array size: ", E_avg_ij.shape
+        print "Requested modes (greatest-to-largest, one-based-indexing):", modes_requested
+        modes_requested = [Ncoarse - mode for mode in modes_requested]
 	
-	print "Computing time-average for each site..."
-	for i in xrange(Nsites):
-		for j in xrange(Ncoarse):
-			x = E_t_ij[:,i,j]
-			E_avg_ij[i][j] = np.mean(E_t_ij[:,i,j])
+	print "Computing time-average for each site O(T)..."
+        E_avg_ij = np.mean(E_t_ij, axis=0)
 
-
-#	print "Compute dE on each mode, at each time..."
-#	for t, Eij in enumerate(E_t_ij):
-#		if t%100 == 0:
-#			print "t =",t
-#		DeltaEmodes_t_in.append([])
-#		DeltaEtot_t_i.append([])
-#		for i, Ej in enumerate(Eij):
-#			DeltaEmodes_t_in[t].append([])
-#			for n, Mj in enumerate(modes_inj[i]):
-#				weight_n = sum(Mj)
-#				DeltaEmodes_t_in[t][i].append( (np.dot(Ej, Mj) - np.dot(E_avg_ij[i], Mj)) * weight_n )
-#			DeltaEtot_t_i[t].append(sum(Ej))
-
+    
 	# First, check that the modes are normalized, then weight the modes
 	print "Checking normalization and applying weights..."
 	for m_nj in modes_inj:
@@ -179,30 +229,20 @@ def DeltaModeTracker(E_t_ij, modes_inj):
 			assert abs(LA.norm(m_j) - 1) < 1E-6
 			m_j *= sum(m_j)
 	
-	#Then, vectorize the code:
-	print "Running vectorized mode tracker computation..."
-	dEmode_i_tm = []
-	for i in xrange(Nsites):
-		y = np.matrix(np.inner(E_avg_ij[i,:], modes_inj[i,:,:]))
-			# y is (1,m)
-		x = np.matrix(np.inner(E_t_ij[:,i,:], modes_inj[i,:,:]))
-			# x is (t,m)
-
-		# Matrix subtraction will repeat rows of y to subtract from x
-		dEmode_i_tm.append(np.array(x - y))
+        return_modes_nt = np.zeros((len(modes_requested), Ntimes))
+	#Then, compute mode timeseries:
+	print "Running vectorized mode tracker computation (O(T*num(modes_requested)))..."
+	for i,n in enumerate(modes_requested):
+            return_modes_nt[i,:] = np.inner(E_t_ij[:,site,:], modes_inj[site,n,:]) -\
+                                   np.inner(E_avg_ij[site,:], modes_inj[site,n,:])
 
 
-	print "Running total dE(t) computation..."
-	#   [1 x i] matrix
-	y = np.matrix(np.inner(E_avg_ij, np.ones(Ncoarse)))
-	#   [t x i] matrix           
-	x = np.matrix(np.inner(E_t_ij, np.ones(Ncoarse)))
-	
-	dEtot_t_i = np.array(x - y)
+	print "Running residual dDE(t) computation (O(T))..."
+        dDEresidual_t = np.sum(E_t_ij[:,site,:], axis=1) - np.sum(E_avg_ij[site,:]) - np.sum(return_modes_nt, axis=0)
 
 	print "Done."
 
-	return dEmode_i_tm, dEtot_t_i
+	return return_modes_nt, dDEresidual_t
 
 
 
@@ -215,11 +255,20 @@ def DeltaModeTracker(E_t_ij, modes_inj):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Compute the eigenvalues of the correlation matrix produced by SidechainCorr, then plot the timeseries for the modes selected")
-    parser.add_argument("--plotspectrum", choices=[None,'pdf','png','display'], help="Select destination for plotted spectrum")
+    parser = argparse.ArgumentParser(description="Compute the eigenvalues of the correlation matrix produced by SidechainCorr, then plot the timeseries for the modes selected. Leaves the database unmodified.")
+    parser.add_argument("site", type=int, help="Site that the data is requested for, use 1-based indexing. No error checking.")
     parser.add_argument("--outfnamebase", help="Filename base for output from plotspectrum and other outputs")
+    parser.add_argument("--plotspectrum", choices=[None,'pdf','png','display'], help="Select destination for plotted spectrum")
+    parser.add_argument("--plotdEt",      choices=[None,'pdf','png','display'], help="Select destination for plotted spectrum")
+    parser.add_argument("--dEtmodes", type=int, nargs='+', action='append',     help="(requires plot dEt) A collection of all modes to include in the timeseries, using zero-based indexing.")
+    parser.add_argument("--plot2dhist",   choices=[None,'pdf','png','display'], help="Select destination for anharmonic scatterplot")
+    parser.add_argument("--modes2d", type=int, nargs=2, action='append',        help="(requires plot2dhist) Selecting the mode-pair for 2d histogram. Repeat this option for multiple plots.")
+    parser.add_argument("--plot1dhist",   choices=[None,'pdf','png','display'], help="Select destination for anharmonic scatterplot")
+    parser.add_argument("--modes1d", type=int, nargs='+', action='append',       help="(requires plot1dhist) Select modes to histogram together. Modes in the same option will be plotted together. Repeat this option for multiple plots.")
 
     args = parser.parse_args()
+    print args
+    args.site -= 1
 
     config = ConfigParser.RawConfigParser()
     config.read('./f0postProcess.cfg')
@@ -227,10 +276,11 @@ def main():
     timetag = config.get('sidechain','time_h5tag')
     corrtag = config.get('sidechain','corr_h5tag')
     
-    save_mode_ts = config.getboolean('modes','save_mode_ts')
-    plot_spectrum = config.getboolean('modes','plot_spectrum')
-    modes_pkl = config.get('modes','modes_pkl')
-    time_max = config.getint('modes','time_max')
+    # NOTE: From old version
+    #save_mode_ts = config.getboolean('modes','save_mode_ts')
+    #plot_spectrum = config.getboolean('modes','plot_spectrum')
+    #modes_pkl = config.get('modes','modes_pkl')
+    #time_max = config.getint('modes','time_max')
     
     
     with h5py.File(h5file,'r') as f:
@@ -245,32 +295,45 @@ def main():
         if args.plotspectrum:
             print "Plotting spectrum..."
             if args.outfnamebase:
-       	        PlotLogSpectrum(vi, impact_i, plottype=args.plotspectrum, fnamebase=args.outfnamebase)
+       	        PlotLogSpectrum(site, vi, impact_i, plottype=args.plotspectrum, fnamebase=args.outfnamebase)
             else:
-       	        PlotLogSpectrum(vi, impact_i, plottype=args.plotspectrum)
+       	        PlotLogSpectrum(site, vi, impact_i, plottype=args.plotspectrum)
+        
+        if args.plot2dhist:
+            for modepair in args.modes2d:
+                print "Plotting 2D histogram for site {}...".format(site)
+                dDE_nt,_ = DeltaModeTracker(E_t_ij, wi, args.site, modepair)
+                Plot2DHist(dDE_nt[0], dDE_nt[1], site=site, mode1=modepair[0], mode2=modepair[2], plottype = args.plot2dhist, fnamebase=args.outfnamebase)
+               
+        if args.dEtmodes:
+            dDE_nt, residual_t = DeltaModeTracker(E_t_ij, wi, args.site, args.dEtmodes)
+
+        if args.plot1dhist:
+            raise NotImplementedError("plot1dhist not yet available")
+
 
     
-        if not save_mode_ts:
-            print "save_mode_ts == no, exiting TrackModes.main()"
-            exit(101)
-        else:
-            print "save_mode_ts = yes, saving the computed trajectories for PlotModeTimeseries..."
-    
-    N = time_max
-    E_t_i = []
-    nread = 0
-    
-    print "Reading",N,"times of sidechain data from "+csv_filename+" to plot timeseries..."
-    E_t_i, nread = SidechainRead(csv_filename, N)
-    print "\tActually read", nread, "times from the file."
-    
-    DeltaEmodes_i_tn, DeltaEtot_t_i = np.array(DeltaModeTracker(E_t_i, wi))
-    DeltaEmodes_i_tn = np.array(DeltaEmodes_i_tn)
-    DeltaEtot_t_i    = np.array(DeltaEtot_t_i)
-    print "Dumping to .pkl file..."
-    cPickle.dump( (DeltaEmodes_i_tn, DeltaEtot_t_i), open(modes_pkl,"w") );
-    
-    print "TrackModes.py complete."
+    # NOTE: From old version
+    #if not save_mode_ts:
+    #    print "save_mode_ts == no, exiting TrackModes.main()"
+    #    exit(101)
+    #else:
+    #    print "save_mode_ts = yes, saving the computed trajectories for PlotModeTimeseries..."
+    #N = time_max
+    #E_t_i = []
+    #nread = 0
+    #
+    #print "Reading",N,"times of sidechain data from "+csv_filename+" to plot timeseries..."
+    #E_t_i, nread = SidechainRead(csv_filename, N)
+    #print "\tActually read", nread, "times from the file."
+    #
+    #DeltaEmodes_i_tn, DeltaEtot_t_i = np.array(DeltaModeTracker(E_t_i, wi))
+    #DeltaEmodes_i_tn = np.array(DeltaEmodes_i_tn)
+    #DeltaEtot_t_i    = np.array(DeltaEtot_t_i)
+    #print "Dumping to .pkl file..."
+    #cPickle.dump( (DeltaEmodes_i_tn, DeltaEtot_t_i), open(modes_pkl,"w") );
+    #
+    #print "TrackModes.py complete."
 
 if __name__ == "__main__":
 	main()

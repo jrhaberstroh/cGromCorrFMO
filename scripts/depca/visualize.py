b1a0086c748b890003bc610b4ea24ba5543c95f0
John Haberstroh <jhaberstroh@berkeley.edu>
2014-10-18 18:34:59 -0700

    Included system module, beginning work on visualization

---
+++ scripts/depca/visualize.py
@@ -0,0 +1,11 @@
+import depca.visualize.pcaviz as pca
+import depca.system as system
+
+def PlotGapDist():
+    system = dESystem('./f0postProcess.cfg')
+    
+
+
+
+if __name__=="__main__":
+    PlotGapDist()
